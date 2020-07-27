077f14cccdee9026476c42554692656bb4a6b2c5
Taejun Lim <tj5054@icloud.com>
2020-07-27 17:25:49 +0900

    Initial commit

---
+++ SharingCharger/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SharingCharger
+//
+//  Created by tjlim on 2020/07/27.
+//  Copyright © 2020 metisinfo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
