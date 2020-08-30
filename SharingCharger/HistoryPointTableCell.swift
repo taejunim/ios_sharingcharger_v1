ed76dfd1403c1d884b25a4ac0d4f7eb92548aac1
hong <clghd0602@gmail.com>
2020-08-30 12:36:22 +0900

    포인트 이력 작업중.

---
+++ SharingCharger/HistoryPointTableCell.swift
@@ -0,0 +1,21 @@
+//
+//  HistoryPointTableCell.swift
+//  SharingCharger
+//
+//  Created by chihong an on 2020/08/27.
+//  Copyright © 2020 metisinfo. All rights reserved.
+//
+
+import UIKit
+
+
+class HistoryPointTableCell:UITableViewCell {
+    
+
+    @IBOutlet weak var pointStateNm: UILabel!
+    @IBOutlet weak var usePoint: UILabel!
+    @IBOutlet weak var pointDate: UILabel!
+    @IBOutlet weak var currentPoint: UILabel!
+    
+    
+}
---
+++ SharingCharger/HistoryPointViewController.swift
@@ -0,0 +1,89 @@
+//
+//  HistoryPointViewController.swift
+//  SharingCharger
+//
+//  Created by chihong an on 2020/08/27.
+//  Copyright © 2020 metisinfo. All rights reserved.
+//
+
+import UIKit
+
+class HistoryPointViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    
+    @IBOutlet weak var tableView: UITableView!
+    
+
+    var arr:Array = ["1","2"]
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        self.tableView.delegate = self
+        self.tableView.dataSource = self
+        
+        self.tableView.allowsSelection = false
+        
+        self.navigationController?.navigationBar.tintColor = UIColor.black  //백버튼 검은색으로
+        self.navigationController?.navigationBar.topItem?.title = ""        //백버튼 텍스트 제거
+        
+        let rightBarButton = UIBarButtonItem.init(title: "right", style: .done, target: self, action: #selector(rightMenu))
+
+        
+        navigationItem.rightBarButtonItem = rightBarButton
+    }
+    
+    
+    
+    override func viewWillAppear(_ animated: Bool) {
+                
+        super.viewWillAppear(animated)
+    }
+    
+
+    @objc func rightMenu() {
+        
+        guard let uvc = self.storyboard?.instantiateViewController(withIdentifier: "Setting") else { return }
+            
+        self.navigationController?.pushViewController(uvc, animated: true)
+    }
+    
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        
+        return arr.count
+    }
+    
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        
+        return 1
+    }
+    
+    
+    
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let Cell = tableView.dequeueReusableCell(withIdentifier: "CustomCell", for:indexPath)
+        
+        return Cell
+    }
+    
+    
+    
+    
+
+    
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
