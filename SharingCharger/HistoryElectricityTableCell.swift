8f5a8c2fbb39c00884116617bc8f3c393cc1d3c9
hong <clghd0602@gmail.com>
2020-08-31 11:48:09 +0900

    포인트 이력 작업중.

---
+++ SharingCharger/HistoryElectricityTableCell.swift
@@ -0,0 +1,30 @@
+//
+//  HistoryElectricityTableCellViewController.swift
+//  SharingCharger
+//
+//  Created by chihong an on 2020/08/31.
+//  Copyright © 2020 metisinfo. All rights reserved.
+//
+
+import UIKit
+
+class HistoryElectricityTableCellViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
