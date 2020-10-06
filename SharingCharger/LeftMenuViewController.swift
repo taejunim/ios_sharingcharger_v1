2e4b8f39ed508dc23912659dd06a999cdcac2fc5
joyouyeong <dbdud2407@gmail.com>
2020-10-06 11:36:02 +0900

    예약진행 ui 완료

--- SharingCharger/LeftMenuViewController.swift
+++ SharingCharger/LeftMenuViewController.swift
@@ -10,21 +10,26 @@ import UIKit
 
 class LeftMenuViewController: UIViewController {
 
+    @IBOutlet var pointCharge: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        initializeLeftMenu()
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    private func initializeLeftMenu() {
+        
+        //print("LeftMenuViewController - initializeLeftMenu")
+        
+        pointCharge.addTarget(self, action: #selector(pointChargeButton(sender:)), for: .touchUpInside)
     }
-    */
+    
+    @objc func pointChargeButton(sender: UIView!) {
+        
+        print("충전하기 이벤트")
+
+    }
+    
 
 }
--- SharingCharger/ReservationViewController.swift
+++ SharingCharger/ReservationViewController.swift
@@ -10,8 +10,32 @@ import UIKit
 
 class ReservationViewController: UIViewController {
 
+    @IBOutlet weak var myPoint: UILabel!
+    @IBOutlet weak var confirmReservation: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+
+        initializeCheckReservation()
+    }
+    
+    private func initializeCheckReservation() {
+        
+        setMyPointLabel()
+        
+        confirmReservation.layer.cornerRadius = 7
+        confirmReservation.addTarget(self, action: #selector(confirmReservation(sender:)), for: .touchUpInside)
+    }
+    
+    @objc func setMyPointLabel() {
+        
+        myPoint.layer.cornerRadius = 7
+
+    }
+    
+    @objc func confirmReservation(sender: UIView!) {
+        
+        print("예약완료 이벤트")
 
     }
 }
